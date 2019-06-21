334c0183821e10e215b2501daa0928a23edfdcb3
Duncan McGregor <duncan@oneeyedmen.com>
2019-06-21 17:29:13 +0100

    Next

--- test/TennisScoreTests.kt
+++ test/TennisScoreTests.kt
@@ -6,8 +6,15 @@ class TennisScoreTests {
     @Test fun `0 - 0 is love all`() {
         assertEquals("love all", Score(0, 0).toString())
     }
+
+    @Test fun `1 - 0 is 15 love`() {
+        assertEquals("15 love", Score(1, 0).toString())
+    }
 }
 
-class Score(p1: Int, p2: Int) {
-    override fun toString(): String = "love all"
+class Score(private val p1: Int, private val p2: Int) {
+    override fun toString(): String = when {
+        p1 == 0 && p2 == 0 -> "love all"
+        else -> "15 love"
+    }
 }
