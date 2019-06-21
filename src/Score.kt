4c36627d15f58d5684e4233e926ba5358de74955
Duncan McGregor <duncan@oneeyedmen.com>
2019-06-21 17:58:02 +0100

    Next

--- src/Score.kt
+++ src/Score.kt
@@ -4,7 +4,6 @@ class Score(private val p1: Int, private val p2: Int) {
         p1 == p2 -> "${p1.toTennis()} all"
         p1 >= 4 && p2 == p1 - 1 -> "advantage player 1"
         p2 >= 4 && p1 == p2 - 1 -> "advantage player 2"
-        p1 == 4 && p2 == 5 -> "advantage player 2"
         else -> "${p1.toTennis()} ${p2.toTennis()}"
     }
 }
