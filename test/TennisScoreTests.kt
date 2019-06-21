7797f136bfbc468d6df0ca2ee52503035ad07eb9
Duncan McGregor <duncan@oneeyedmen.com>
2019-06-21 17:27:38 +0100

    Next

--- test/TennisScoreTests.kt
+++ test/TennisScoreTests.kt
@@ -1,9 +1,13 @@
 import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
 
 class TennisScoreTests {
 
-    @Test fun test() {
-
-
+    @Test fun `0 - 0 is love all`() {
+        assertEquals("love all", Score(0, 0).toString())
     }
+}
+
+class Score(p1: Int, p2: Int) {
+    override fun toString(): String = "love all"
 }
