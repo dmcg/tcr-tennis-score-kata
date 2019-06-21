5c15265a3e7b08036b8bb9ec534f926b397fde21
Duncan McGregor <duncan@oneeyedmen.com>
2019-06-21 17:37:47 +0100

    Next

--- src/Score.kt
+++ src/Score.kt
@@ -1,8 +1,8 @@
 class Score(private val p1: Int, private val p2: Int) {
     override fun toString(): String = when {
         p1 == 0 && p2 == 0 -> "${p1.toTennis()} all"
-        p1 == 1 && p2 == 0 -> "15 love"
-        p1 == 0 && p2 == 1 -> "love 15"
+        p1 == 1 && p2 == 0 -> "${p1.toTennis()} ${p2.toTennis()}"
+        p1 == 0 && p2 == 1 -> "${p1.toTennis()} ${p2.toTennis()}"
         p1 == 1 && p2 == 1 -> "15 all"
         else -> TODO()
     }
